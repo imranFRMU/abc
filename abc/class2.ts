49edf6551cec3503549bea60303642ec35e6a01b
imranFRMU <162621539+imranFRMU@users.noreply.github.com>
2024-03-15 02:59:37 -0700

    Add files via upload

---
+++ abc/class2.ts
@@ -0,0 +1,56 @@
+// var name1="imran ahmed"
+// console.log(name1)
+// var age = "23-Dec-1989"
+// console.log(age)
+
+// let name2 = "Rafiq Ullah Khan"
+// console.log(name2)
+// let age = "23-Dec-1980"
+// console.log(age)
+
+// let name2 = "Imran Ahmed"
+// let age = "23-Dec-1978"
+// console.log(name2,age)
+
+
+// let ramzangreetings = "Ramzan Mubarak *****"
+// let person = "To All"
+// console.log(ramzangreetings,person)
+
+// var name1="Kamran Abbas";
+// console.log(name1)
+
+// let name2 = "Kamran Abbas"
+// let age = "11 june 1974"
+//console.log (name2,age)
+
+// let ramzangreetings = "Ramzan Mubarak"
+// let person = "To All of You"
+// console.log(ramzangreetings,person)
+
+// let comments = "Long Live Pakistan"
+// console.log(comments)
+
+// let name2 = "Karachi"
+// let age = "since 1947"
+// console.log(name2,age)
+
+// var name5 = "Apna Karachi.com";
+// var age ="1990";
+// console.log(name5,age)
+
+// let arrivaloftimings = "09:30 to 10:30"
+// let person = "all staff"
+// console.log(arrivaloftimings,person)
+
+//let birthdaywishes = "Happy Birth Day"
+//let name1 = "Imran Ahmed Khan Niazi"
+//console.log(birthdaywishes,name1)
+
+// var name8="Samba Bank"
+// var since ="1997"
+// console.log(name8,since)
+
+let arrivaltiming ="09:30"
+let departuretiming ="10:30"
+console.log (arrivaltiming, departuretiming)
